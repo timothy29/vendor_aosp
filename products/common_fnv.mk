5eb536f7358cc23ba1252289e5d2f08d3e16669f
remicks <lost.junglist@gmail.com>
2012-08-01 07:49:50 -0500

    Add GooManager Support and Black Exodus theme
    Signed-off-by: remicks <lost.junglist@gmail.com>

--- products/common_fnv.mk
+++ products/common_fnv.mk
@@ -7,3 +7,20 @@ PRODUCT_PACKAGES += \
 # Build Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.fnv.version=$(TARGET_PRODUCT)_008
+
+# GooManager Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=projectfnv \
+    ro.goo.rom=FNV \
+    ro.goo.version=1
+
+# Copy Black Exodus to /system
+PRODUCT_COPY_FILES += \
+    vendor/fnv/proprietary/common/app/BlackExodus.apk:system/app/com.nitroz.blackexodus.apk
+
+# Set Default Theme Chooser Theme
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.themeId=BlackExodus \
+    persist.sys.themePackageName=com.nitroz.blackexodus
+
+    
