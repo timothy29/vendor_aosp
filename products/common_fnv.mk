2dd0c7398a487b4ae1e6d66f9d3c1f54c6c018e6
remicks <lost.junglist@gmail.com>
2012-08-06 14:19:03 -0500

    New PCB Carrot theme by scar45
    Signed-off-by: remicks <lost.junglist@gmail.com>

--- products/common_fnv.mk
+++ products/common_fnv.mk
@@ -17,11 +17,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Copy themes to /system
 PRODUCT_COPY_FILES += \
     vendor/fnv/proprietary/common/app/BlackExodus.apk:system/app/com.nitroz.blackexodus.apk \
-    vendor/fnv/proprietary/common/app/FreehandWhite-release.apk:system/app/com.hooolm.freehand.ics.white.apk
+    vendor/fnv/proprietary/common/app/FreehandWhite-release.apk:system/app/com.hooolm.freehand.ics.white.apk \
+    vendor/fnv/proprietary/common/app/pcb-carrot-release.apk:system/app/com.teambroccoli.theme.pcbcarrot.apk
 
 # Set Default Theme Chooser Theme
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.themeId=FreehandWhite \
-    persist.sys.themePackageName=com.hooolm.freehand.ics.white
+    persist.sys.themeId=PCBCarrot \
+    persist.sys.themePackageName=com.teambroccoli.theme.pcbcarrot
 
     
