1d51c3c58841e1ded96d8bc34e9ed50dd2f14a97
remicks <lost.junglist@gmail.com>
2012-08-12 13:59:41 -0500

    Add Beets theme and make default
    Signed-off-by: remicks <lost.junglist@gmail.com>

--- products/common_fnv.mk
+++ products/common_fnv.mk
@@ -18,11 +18,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/fnv/proprietary/common/app/BlackExodus.apk:system/app/com.nitroz.blackexodus.apk \
     vendor/fnv/proprietary/common/app/FreehandWhite-release.apk:system/app/com.hooolm.freehand.ics.white.apk \
-    vendor/fnv/proprietary/common/app/pcb-carrot-release.apk:system/app/com.teambroccoli.theme.pcbcarrot.apk
+    vendor/fnv/proprietary/common/app/pcb-carrot-release.apk:system/app/com.teambroccoli.theme.pcbcarrot.apk \
+    vendor/fnv/proprietary/common/app/com.knokfirst.BeetsbyKnokDonate.apk:system/app/com.knokfirst.BeetsbyKnokDonate.apk
 
 # Set Default Theme Chooser Theme
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.themeId=PCBCarrot \
-    persist.sys.themePackageName=com.teambroccoli.theme.pcbcarrot
+    persist.sys.themeId=BeetsbyKnok \
+    persist.sys.themePackageName=com.knokfirst.BeetsbyKnokDonate
 
     
