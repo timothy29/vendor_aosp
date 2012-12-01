82058447ff76a9eac576fd7e82855e814299d161
Daniel <daxx4182@gmail.com>
2012-12-01 18:42:14 +0800

    Fast bump, new release
    Change-Id: I800d20baca71b28d49aab17e4cfe74507126f1f7

--- configs/common.mk
+++ configs/common.mk
@@ -5,7 +5,7 @@ PRODUCT_DEVICE := generic
 
 # Build Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)_0083_$(shell date +"%m-%d-%Y")
+    ro.xylon.version=$(TARGET_PRODUCT)_009_$(shell date +"%m-%d-%Y")
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
 
