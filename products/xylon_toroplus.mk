7f8e33507b5c7d915b90326eff16cac0d001a3d0
Daniel <daxx4182@gmail.com>
2012-12-07 23:27:41 +0800

    I like to break stuffs.

---
+++ products/xylon_toroplus.mk
@@ -0,0 +1,25 @@
+# Inherit AOSP device configuration for toro.
+$(call inherit-product, device/samsung/toroplus/full_toroplus.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Inherit fnv specific configurations
+
+# Setup device specific product configuration.
+PRODUCT_NAME := xylon_toroplus
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := toroplus
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.1.1/JRO03U/L700FH05:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.1.1 JRO03U L700FH05 release-keys"
+
+# Tuna Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/tuna
+
+# Copy toro specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/aosp/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 add_lunch_combo xylon_maguro-userdebug
-add_lunch_combo xylon_toro-userdebug
 add_lunch_combo xylon_mako-userdebug
+add_lunch_combo xylon_toro-userdebug
+add_lunch_combo xylon_toroplus-userdebug
