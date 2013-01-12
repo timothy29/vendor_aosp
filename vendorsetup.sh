a8cb7a0d0de1210c3336e6cda98fbf556f07b436
netchip <vanderpoel24@gmail.com>
2013-01-12 09:39:50 +0000

    Add Samsung GT-I9300

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,4 @@ add_lunch_combo xylon_mako-userdebug
 add_lunch_combo xylon_tilapia-userdebug
 add_lunch_combo xylon_toro-userdebug
 add_lunch_combo xylon_toroplus-userdebug
+add_lunch_combo xylon_i9300-userdebug
