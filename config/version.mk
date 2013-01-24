b053eb4afd933cd4a26faaacc1811cf88a9fa278
Daniel <daxx4182@gmail.com>
2013-01-24 20:56:51 +0800

    SWAAAG 017-1

--- config/version.mk
+++ config/version.mk
@@ -3,13 +3,13 @@ DATE = $(shell date -u +%Y%m%d)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)-official-017 \
-    ro.modversion=$(TARGET_PRODUCT)_017_$(DATE)
+    ro.xylon.version=$(TARGET_PRODUCT)-official-017-1 \
+    ro.modversion=$(TARGET_PRODUCT)_017-1_$(DATE)
 
 # For XYStats
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xylon.isitrom=Xylon \
     ro.xylon.modname=xylon-aosp \
-    ro.xylon.modversion=Official_017 \
+    ro.xylon.modversion=Official_017-1 \
     ro.xylon.device=$(TARGET_PRODUCT) \
     ro.xylon.builtdate=$(DATE)
