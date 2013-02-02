2bd09380d15d6bcdb7e126e9f4a5b1f9a326d966
Daniel <daxx4182@gmail.com>
2013-02-02 11:57:32 +0800

    derp state

--- products/p3100.mk
+++ products/p3100.mk
@@ -1,10 +1,10 @@
-# Inherit AOSP device configuration for grouper.
+# Inherit AOSP device configuration for GT-P3100.
 $(call inherit-product, device/samsung/p3100/full_p3100.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/aosp/config/common.mk)
 
-# Tilapia Overlay
+# GT-P3100 Overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/tilapia
 
 # Inherit GSM common stuff
@@ -22,7 +22,7 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=espressorfxx BUILD_FINGERPRINT=samsung/espressorfxx/espressorf:4.0.3/IML74K/P3100XWALE2:user/release-keys PRIVATE_BUILD_DESC="espressorfxx-user 4.0.3 IML74K P3100XWALE2 release-keys"
 
-# Copy maguro specific prebuilt files
+# Boot Animation
 PRODUCT_COPY_FILES +=  \
     vendor/aosp/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip \
     vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
