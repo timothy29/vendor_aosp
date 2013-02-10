fac5170354a8413ab422f585c61b80401619d32c
Daniel <daxx4182@gmail.com>
2013-02-10 14:10:26 +0800

    019.1

--- config/version.mk
+++ config/version.mk
@@ -3,13 +3,13 @@ DATE = $(shell date -u +%Y%m%d)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)-official-019 \
-    ro.modversion=$(TARGET_PRODUCT)_019_$(DATE)
+    ro.xylon.version=$(TARGET_PRODUCT)-official-019.1 \
+    ro.modversion=$(TARGET_PRODUCT)_019.1_$(DATE)
 
 # For XYStats
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xylon.isitrom=Xylon \
     ro.xylon.modname=xylon-aosp \
-    ro.xylon.modversion=Official_019 \
+    ro.xylon.modversion=Official_019.1 \
     ro.xylon.device=$(TARGET_PRODUCT) \
     ro.xylon.builtdate=$(DATE)
