159d92dbc8302b04a08260a68c9b51a8995bd7e3
Daniel <daxx4182@gmail.com>
2013-02-16 15:13:58 +0800

    Rebranding version number

--- config/version.mk
+++ config/version.mk
@@ -3,13 +3,13 @@ DATE = $(shell date -u +%Y%m%d)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)-official-020 \
-    ro.modversion=$(TARGET_PRODUCT)_020_$(DATE)
+    ro.xylon.version=$(TARGET_PRODUCT)-official-2.1 \
+    ro.modversion=$(TARGET_PRODUCT)_2.1_$(DATE)
 
 # For XYStats
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xylon.isitrom=Xylon \
     ro.xylon.modname=xylon-aosp \
-    ro.xylon.modversion=Official_020 \
+    ro.xylon.modversion=Official_2.1 \
     ro.xylon.device=$(TARGET_PRODUCT) \
     ro.xylon.builtdate=$(DATE)
