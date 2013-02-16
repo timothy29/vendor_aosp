f0e4393a2de864f33a3998aba889ec156fd6ecff
Daniel <daxx4182@gmail.com>
2013-02-16 20:05:54 +0800

    Fix system syntax error

--- config/buildprop_common.mk
+++ config/buildprop_common.mk
@@ -2,15 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
     windowsmgr.max_events_per_sec=280 \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=96m \
-    dalvik.vm.heapsize=256m \
-    dalvik.vm.dexopt-flags=m=y,v=n,o=v \
-    dalvik.vm.execution-mode=int:jit \
-    dalvik.vm.lockprof.threshold=850 \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.verify-bytecode=false \
-    dalvik.vm.stack-trace-file=/data/anr/traces.txt \
     ro.media.enc.jpeg.quality=100 \
     ro.HOME_APP_ADJ=1 \
     ro.media.dec.jpeg.memcap=8000000 \
@@ -34,7 +25,6 @@ PRODUCT_COPY_FILES +=  \
     vendor/aosp/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
     vendor/aosp/prebuilt/common/etc/init.d/00fnvxy:system/etc/init.d/00fnvxy \
     vendor/aosp/prebuilt/common/etc/init.d/01cherrybomb:system/etc/init.d/01cherrybomb \
-    vendor/aosp/prebuilt/common/etc/init.d/02cleaning:system/etc/init.d/02cleaning \
     vendor/aosp/prebuilt/common/etc/init.d/95zipalign:system/etc/init.d/95zipalign \
     vendor/aosp/prebuilt/common/etc/init.d/98fruit:system/etc/init.d/98fruit \
     vendor/aosp/prebuilt/common/bin/zipalign:system/bin/zipalign
