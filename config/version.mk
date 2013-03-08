075ad97abe65e26b568207dececb7c56b70bff36
Daniel <daxx4182@gmail.com>
2013-03-08 15:07:14 +0800

    2.2.1
    Signed-off-by: Daniel <daxx4182@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -3,13 +3,13 @@ DATE = $(shell date -u +%Y%m%d)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)-official-2.2 \
-    ro.modversion=$(TARGET_PRODUCT)_2.2_$(DATE)
+    ro.xylon.version=$(TARGET_PRODUCT)-official-2.2.1 \
+    ro.modversion=$(TARGET_PRODUCT)_2.2.1_$(DATE)
 
 # For XYStats
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xylon.isitrom=xylon \
     ro.xylon.modname=xylon-aosp \
-    ro.xylon.modversion=2.2-$(DATE) \
+    ro.xylon.modversion=2.2.1-$(DATE) \
     ro.xylon.device=$(TARGET_PRODUCT) \
     ro.xylon.builtdate=$(DATE)
