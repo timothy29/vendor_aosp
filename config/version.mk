12085aa8a7d0a32cbb6a47e2cc6aac83f80d6d6e
Daniel <daxx4182@gmail.com>
2013-03-26 17:36:13 +0800

    2.4 beta with hybrid

--- config/version.mk
+++ config/version.mk
@@ -3,13 +3,13 @@ DATE = $(shell date -u +%Y%m%d)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)-official-2.3.3 \
-    ro.modversion=$(TARGET_PRODUCT)_2.3.3_$(DATE)
+    ro.xylon.version=$(TARGET_PRODUCT)-official-2.4-beta \
+    ro.modversion=$(TARGET_PRODUCT)_2.4-beta_$(DATE)
 
 # For XYStats
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xylon.isitrom=xylon \
     ro.xylon.modname=xylon-aosp \
-    ro.xylon.modversion=2.3.3-$(DATE) \
+    ro.xylon.modversion=2.4-beta-$(DATE) \
     ro.xylon.device=$(TARGET_PRODUCT) \
     ro.xylon.builtdate=$(DATE)
