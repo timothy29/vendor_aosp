2bf6aee6e65f56900742674d9b4c6e22c36e1d83
Daniel <daxx4182@gmail.com>
2013-03-31 10:32:32 +0800

    Make xHDPI's SysUI cute

--- config/version.mk
+++ config/version.mk
@@ -3,13 +3,13 @@ DATE = $(shell date -u +%Y%m%d)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)-official-2.4-beta2 \
-    ro.modversion=$(TARGET_PRODUCT)_2.4-beta2_$(DATE)
+    ro.xylon.version=$(TARGET_PRODUCT)-official-2.4 \
+    ro.modversion=$(TARGET_PRODUCT)_2.4_$(DATE)
 
 # For XYStats
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xylon.isitrom=xylon \
     ro.xylon.modname=xylon-aosp \
-    ro.xylon.modversion=2.4-beta2-$(DATE) \
+    ro.xylon.modversion=2.4-$(DATE) \
     ro.xylon.device=$(TARGET_PRODUCT) \
     ro.xylon.builtdate=$(DATE)
