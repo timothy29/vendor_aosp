a574978f091cbab6875170dc1bda7474350b9bc3
Daniel <daxx4182@gmail.com>
2013-04-12 13:01:53 +0800

    2.5 bump

--- config/version.mk
+++ config/version.mk
@@ -3,13 +3,13 @@ DATE = $(shell date -u +%Y%m%d)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)-official-2.4.2 \
-    ro.modversion=$(TARGET_PRODUCT)_2.4.2_$(DATE)
+    ro.xylon.version=$(TARGET_PRODUCT)-official-2.5 \
+    ro.modversion=$(TARGET_PRODUCT)_2.5_$(DATE)
 
 # For XYStats
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xylon.isitrom=xylon \
     ro.xylon.modname=xylon-aosp \
-    ro.xylon.modversion=2.4.2-$(DATE) \
+    ro.xylon.modversion=2.5-$(DATE) \
     ro.xylon.device=$(TARGET_PRODUCT) \
     ro.xylon.builtdate=$(DATE)
