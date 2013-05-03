5e3895197d3ac393d8f8c502e6397ff7684a6135
Daniel <daxx4182@gmail.com>
2013-05-03 19:24:14 +0800

    derp

--- products/m7.mk
+++ products/m7.mk
@@ -1,5 +1,5 @@
 # Inherit AOSP device configuration for mako.
-$(call inherit-product, device/htc/device_m7.mk)
+$(call inherit-product, device/htc/m7/device_m7.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/aosp/config/common.mk)
