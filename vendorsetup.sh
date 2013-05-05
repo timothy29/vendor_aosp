3af9feab4d92ab150500ccea0ece3bcd4950f49e
David Hou <david.hou314@gmail.com>
2013-05-05 14:28:30 -0400

    derp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,6 +17,7 @@ add_lunch_combo xylon_i9300-userdebug
 add_lunch_combo xylon_i9305-userdebug
 add_lunch_combo xylon_jfltetmo-userdebug
 add_lunch_combo xylon_jflteatt-userdebug
+add_lunch_combo xylon_jfltecan-userdebug
 add_lunch_combo xylon_n7100-userdebug
 add_lunch_combo xylon_p3100-userdebug
 add_lunch_combo xylon_leo-userdebug
